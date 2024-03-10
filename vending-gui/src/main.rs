390aa3dcb2cd5d1b49beaeb07f043c9f4b3ec9f6
Aaron Thapa <notsmart98@gmail.com>
2024-03-10 20:33:29 +0545

    hello world

--- vending-gui/src/main.rs
+++ vending-gui/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
+        ..Default::default()
+    };
+
+    eframe::run_simple_native("hello world", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+        });
+    })
 }
