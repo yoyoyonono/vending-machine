287309755d4dadf04fbe4f9c6146aeb3737f7f72
Aaron Thapa <notsmart98@gmail.com>
2024-03-10 21:58:48 +0545

    extract button

--- vending-gui/src/main.rs
+++ vending-gui/src/main.rs
@@ -1,5 +1,4 @@
 use eframe::egui;
-use eframe::egui::Widget;
 use egui::vec2;
 
 fn main() -> Result<(), eframe::Error> {
@@ -17,50 +16,69 @@ fn main() -> Result<(), eframe::Error> {
     ))
 }
 
-struct AppState {
-
-}
-
-impl Default for AppState {
-    fn default() -> Self {
-        Self {
-
-        }
-    }
-}
-
 impl eframe::App for AppState {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Select Item");
             egui::Grid::new("some_unique_id").show(ui, |ui| {
-                selection_button(ui, "1");
-                selection_button(ui, "2");
-                selection_button(ui, "3");
-                selection_button(ui, "A");
+                selection_button(self, ui, "1");
+                selection_button(self, ui, "2");
+                selection_button(self, ui, "3");
+                selection_button(self, ui, "A");
                 ui.end_row();
-                selection_button(ui, "4");
-                selection_button(ui, "5");
-                selection_button(ui, "6");
-                selection_button(ui, "B");
+                selection_button(self, ui, "4");
+                selection_button(self, ui, "5");
+                selection_button(self, ui, "6");
+                selection_button(self, ui, "B");
                 ui.end_row();
-                selection_button(ui, "7");
-                selection_button(ui, "8");
-                selection_button(ui, "9");
-                selection_button(ui, "C");
+                selection_button(self, ui, "7");
+                selection_button(self, ui, "8");
+                selection_button(self, ui, "9");
+                selection_button(self, ui, "C");
                 ui.end_row();
-                selection_button(ui, "*");
-                selection_button(ui, "0");
-                selection_button(ui, "#");
-                selection_button(ui, "D");
+                selection_button(self, ui, "*");
+                selection_button(self, ui, "0");
+                selection_button(self, ui, "#");
+                selection_button(self, ui, "D");
                 ui.end_row();
             });
+            ui.label(format!("Selected: {}{}", self.current_selection.letter, self.current_selection.number));
         });
     }
 }
 
-fn selection_button(ui: &mut egui::Ui, text: &str) {
+fn selection_button(state: &mut AppState, ui: &mut egui::Ui, text: &str) {
     if ui.add(egui::Button::new(text).min_size(vec2(50.0, 50.0))).clicked() {
         println!("Button {} clicked", text);
+        match text {
+            "A" => state.current_selection.letter = 'A',
+            "B" => state.current_selection.letter = 'B',
+            "C" => state.current_selection.letter = 'C',
+            "D" => state.current_selection.letter = 'D',
+            "#" => println!("dispense"),
+            "*" => println!("cancel"),
+            _ => state.current_selection.number = text.parse().unwrap(),
+        }
     }
 }
+
+struct Selection {
+    letter: char,
+    number: i32,
+}
+
+struct AppState {
+    current_selection: Selection,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self {
+            current_selection: Selection {
+                letter: 'A',
+                number: 1,
+            },            
+        }
+    }
+}
+
