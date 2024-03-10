d620bd04b00ebeac46bb9a9dd4dd8efe5f332adb
Aaron Thapa <notsmart98@gmail.com>
2024-03-10 20:49:01 +0545

    switch to normal type

--- vending-gui/src/main.rs
+++ vending-gui/src/main.rs
@@ -1,13 +1,36 @@
 use eframe::egui;
 
 fn main() -> Result<(), eframe::Error> {
+    env_logger::init();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
         ..Default::default()
     };
 
-    eframe::run_simple_native("hello world", options, move |ctx, _frame| {
+    eframe::run_native("hello world", options, 
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::<AppState>::default()
+        }
+    ))
+}
+
+struct AppState {
+
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self {
+
+        }
+    }
+}
+
+impl eframe::App for AppState {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+
         });
-    })
+    }
 }
