1e60badce349aafabe113b36260b1da91d29fc74
Aaron Thapa <notsmart98@gmail.com>
2024-03-11 16:09:28 +0545

    more generalizations

--- vending-gui/src/main.rs
+++ vending-gui/src/main.rs
@@ -1,5 +1,5 @@
 use eframe::egui;
-use egui::vec2;
+use egui::Key;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init();
@@ -20,45 +20,58 @@ impl eframe::App for AppState {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Select Item");
-            egui::Grid::new("some_unique_id").show(ui, |ui| {
-                selection_button(self, ui, "1");
-                selection_button(self, ui, "2");
-                selection_button(self, ui, "3");
-                selection_button(self, ui, "A");
-                ui.end_row();
-                selection_button(self, ui, "4");
-                selection_button(self, ui, "5");
-                selection_button(self, ui, "6");
-                selection_button(self, ui, "B");
-                ui.end_row();
-                selection_button(self, ui, "7");
-                selection_button(self, ui, "8");
-                selection_button(self, ui, "9");
-                selection_button(self, ui, "C");
-                ui.end_row();
-                selection_button(self, ui, "*");
-                selection_button(self, ui, "0");
-                selection_button(self, ui, "#");
-                selection_button(self, ui, "D");
-                ui.end_row();
-            });
-            ui.add(egui::Label::new(format!("Selected: {}{}", self.current_selection.letter, self.current_selection.number)));
+            ui.heading(format!("Selected: {}{}", self.current_selection.letter, self.current_selection.number));
+            listen_for_letters(self, ctx);
+            listen_for_numbers(self, ctx);
         });
     }
 }
 
-fn selection_button(state: &mut AppState, ui: &mut egui::Ui, text: &str) {
-    if ui.add(egui::Button::new(text).min_size(vec2(50.0, 50.0))).clicked() {
-        println!("Button {} clicked", text);
-        match text {
-            "A" => state.current_selection.letter = 'A',
-            "B" => state.current_selection.letter = 'B',
-            "C" => state.current_selection.letter = 'C',
-            "D" => state.current_selection.letter = 'D',
-            "#" => println!("dispense"),
-            "*" => println!("cancel"),
-            _ => state.current_selection.number = text.parse().unwrap(),
-        }
+fn listen_for_letters(state: &mut AppState, ctx: &egui::Context) {
+    if ctx.input(|i| i.key_pressed(Key::A)) {
+        state.current_selection.letter = 'A';
+    }
+    if ctx.input(|i| i.key_pressed(Key::B)) {
+        state.current_selection.letter = 'B';
+    }
+    if ctx.input(|i| i.key_pressed(Key::C)) {
+        state.current_selection.letter = 'C';
+    }
+    if ctx.input(|i| i.key_pressed(Key::D)) {
+        state.current_selection.letter = 'D';
+    }
+}
+
+fn listen_for_numbers(state: &mut AppState, ctx: &egui::Context) {
+    if ctx.input(|i| i.key_pressed(Key::Num1)) {
+        state.current_selection.number = 1;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num2)) {
+        state.current_selection.number = 2;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num3)) {
+        state.current_selection.number = 3;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num4)) {
+        state.current_selection.number = 4;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num5)) {
+        state.current_selection.number = 5;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num6)) {
+        state.current_selection.number = 6;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num7)) {
+        state.current_selection.number = 7;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num8)) {
+        state.current_selection.number = 8;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num9)) {
+        state.current_selection.number = 9;
+    }
+    if ctx.input(|i| i.key_pressed(Key::Num0)) {
+        state.current_selection.number = 0;
     }
 }
 
