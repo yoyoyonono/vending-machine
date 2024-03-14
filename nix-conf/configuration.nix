9bb3d2bcc793ab2f9374b27f7eea635e6c0a2052
Aaron Thapa <notsmart98@gmail.com>
2024-03-14 12:10:58 +0545

    change to xfce

--- nix-conf/configuration.nix
+++ nix-conf/configuration.nix
@@ -39,9 +39,14 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+    displayManager.defaultSession = "xfce";
+  };
   
 
   # Configure keymap in X11
