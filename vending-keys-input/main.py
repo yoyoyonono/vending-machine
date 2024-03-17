00cc7a5a1f210ebc5819560f52dd795bc231f484
Aaron Thapa <notsmart98@gmail.com>
2024-03-17 16:23:15 +0545

    keypad example

--- vending-keys-input/main.py
+++ vending-keys-input/main.py
@@ -1,8 +1,20 @@
 from pad4pi import rpi_gpio
 
+KEYPAD = [
+    ["1", "2", "3", "A"],
+    ["4", "5", "6", "B"],
+    ["7", "8", "9", "C"],
+    ["*", "0", "#", "D"]
+]
+
+ROW_PINS = [4, 14, 15, 17] # BCM numbering
+COL_PINS = [18, 27, 22, 23] # BCM numbering
+
 factory = rpi_gpio.KeypadFactory()
 
-factory.create_4_by_4_keypad()
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
 
 def printKey(key):
     print(key)
+
+keypad.registerKeyPressHandler(printKey)
